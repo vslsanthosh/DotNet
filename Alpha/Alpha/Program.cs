a05e4f08eaaf35469c5cd017b6ff2e3108d2dfba
vslsanthosh <vslsanthosh@users.noreply.github.com>
2019-10-13 00:05:39 +0530

    New changes

---
+++ Alpha/Alpha/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Alpha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alpha alpha = new Alpha();
+            Console.WriteLine(alpha.maxPref("zyx"));
+            Console.ReadKey();
+        }
+    }
+
+    public class Alpha
+    {
+        public int maxPref(string s)
+        {
+            char[] cc = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == cc[i]) count++;
+                else return count;
+            }
+            return count;
+        }
+
+
+    }
+}
